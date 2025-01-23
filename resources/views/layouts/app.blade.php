556077f5d786ce51ed5229c58f329219de55321c
IntelligentBeaver <aman.211506@ncit.edu.np>
2025-01-23 23:44:22 +0545

    Few UI Improvements

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,32 +21,79 @@
     </head>
 
     <body class="font-sans antialiased">
-        <nav class="bg-base-100 px-4 py-6">
-            <div class="container mx-auto flex items-center justify-between">
+        <div class="navbar mx-auto max-w-7xl bg-base-100">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <div class="btn btn-ghost lg:hidden" role="button" tabindex="0">
+                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
+                        tabindex="0">
+                        @auth
+                            <li>
+                                <a href="/" wire:navigate>Home</a>
+                            </li>
+                            <li><a class="px-4" href="{{ route('dashboard') }}" wire:navigate>Dashboard</a>
+
+                            </li>
+                            <li>
+                                <form style="display:inline;" method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button class="" type="submit">Logout</button>
+                                </form>
+                            </li>
+                        @else
+                            <li>
+                                <a href="/" wire:navigate>Home</a>
+                            </li>
+                            <li><a class="px-4" href="{{ route('login') }}" wire:navigate>Login</a></li>
+                            <li><a class="px-4" href="{{ route('register') }}" wire:navigate>Register</a></li>
+                        @endauth
+                    </ul>
+                </div>
                 <a class="font-bold" href="/" wire:navigate>
                     <img src="{{ asset('images/logo.png') }}" alt="Logo" width="50" height="30">
                 </a>
-                <div>
-                    @auth
-                        <a class="px-4" href="{{ route('dashboard') }}" wire:navigate>Dashboard</a>
-                        <form style="display:inline;" method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <button class="" type="submit">Logout</button>
-                        </form>
-                    @else
-                        <a class="px-4" href="{{ route('login') }}" wire:navigate>Login</a>
-                        <a class="px-4" href="{{ route('register') }}" wire:navigate>Register</a>
-                    @endauth
-                </div>
             </div>
-        </nav>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal px-1">
+                    @auth
+                        <li>
+                            <a class="px-4" href="{{ route('dashboard') }}" wire:navigate>Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="/" wire:navigate>Home</a>
+                        </li>
+                    @else
+                        <li>
+                            <a href="/" wire:navigate>Home</a>
+                        </li>
+                        <li><a class="px-4" href="{{ route('login') }}" wire:navigate>Login</a></li>
+                        <li><a class="px-4" href="{{ route('register') }}" wire:navigate>Register</a></li>
+                    @endauth
+                </ul>
+            </div>
+            <div class="navbar-end">
+                @auth
+                    <form style="display:inline;" method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button class="btn btn-error text-error-content" type="submit">Logout</button>
+                    </form>
+                @else
+                @endauth
+            </div>
+        </div>
         <main class="mx-auto">
             @yield('content')
         </main>
         <!-- Footer -->
         <footer class="bg-gray-900 px-8 py-6 text-white">
             <div class="text-center">
-                <p class="text-sm">© BrandName. All rights reserved.</p>
+                <p class="text-sm">©VisionCraft. All rights reserved.</p>
             </div>
         </footer>
         @livewireScripts
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -60,18 +60,44 @@
                 <input class="checkbox checkbox-md" type="checkbox" wire:model.blur="remember" checked="checked" />
             </label>
         </div>
-        <button class="btn btn-primary my-3" type="submit">Login</button>
-        <div>
-            <a class="block" href="{{ route('auth.google') }}">
-                <button class="login-with-google-btn" type="button">
-                    Sign in with Google
-                </button>
-            </a>
-            <a class="block" href="{{ route('auth.linkedin') }}">
-                <button class="login-with-google-btn" type="button">
-                    Sign in with LinkedIn
-                </button>
-            </a>
+        <button class="btn btn-primary my-2" type="submit">Login</button>
+        <div class="my-2 flex justify-around gap-4">
+            <div class="flex-1">
+                <a class="btn min-w-full bg-white" href="{{ route('auth.google') }}" role="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                        <path
+                            d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                            fill="#4285F4" />
+                        <path
+                            d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                            fill="#34A853" />
+                        <path
+                            d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                            fill="#FBBC05" />
+                        <path
+                            d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                            fill="#EA4335" />
+                        <path d="M1 1h22v22H1z" fill="none" />
+                    </svg>
+                    Log in with Google
+                </a>
+            </div>
+            <div class="flex-1">
+                <a class="btn min-w-full bg-[#0075b4] text-white" href="{{ route('auth.linkedin') }}" role="button">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 72 72" width="24">
+                        <g fill="none" fill-rule="evenodd">
+                            <path
+                                d="M8,72 L64,72 C68.418278,72 72,68.418278 72,64 L72,8 C72,3.581722 68.418278,-8.11624501e-16 64,0 L8,0 C3.581722,8.11624501e-16 -5.41083001e-16,3.581722 0,8 L0,64 C5.41083001e-16,68.418278 3.581722,72 8,72 Z"
+                                fill="#007EBB" />
+                            <path
+                                d="M62,62 L51.315625,62 L51.315625,43.8021149 C51.315625,38.8127542 49.4197917,36.0245323 45.4707031,36.0245323 C41.1746094,36.0245323 38.9300781,38.9261103 38.9300781,43.8021149 L38.9300781,62 L28.6333333,62 L28.6333333,27.3333333 L38.9300781,27.3333333 L38.9300781,32.0029283 C38.9300781,32.0029283 42.0260417,26.2742151 49.3825521,26.2742151 C56.7356771,26.2742151 62,30.7644705 62,40.051212 L62,62 Z M16.349349,22.7940133 C12.8420573,22.7940133 10,19.9296567 10,16.3970067 C10,12.8643566 12.8420573,10 16.349349,10 C19.8566406,10 22.6970052,12.8643566 22.6970052,16.3970067 C22.6970052,19.9296567 19.8566406,22.7940133 16.349349,22.7940133 Z M11.0325521,62 L21.769401,62 L21.769401,27.3333333 L11.0325521,27.3333333 L11.0325521,62 Z"
+                                fill="#FFF" />
+                        </g>
+                    </svg>
+                    Log in with LinkedIn
+                </a>
+            </div>
         </div>
     </form>
 </div>
